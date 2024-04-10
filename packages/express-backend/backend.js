cf5e37a2243a7537852ada63a10985aabee6b87e
Poornima <poornima.godavarthy@gmail.com>
2024-04-10 09:55:53 -0700

    Backend

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -34,11 +34,25 @@ const users = {
     ]
   };
 
-app.use(express.json());
+  const findUserByName = (name) => {
+    return users["users_list"].filter(
+      (user) => user["name"] === name
+    );
+  };
+  app.use(express.json());
+  
+  app.get("/users", (req, res) => {
+    const name = req.query.name;
+    if (name != undefined) {
+      let result = findUserByName(name);
+      result = { users_list: result };
+      res.send(result);
+    } else {
+      res.send(users);
+    }
+  });
 
-app.get("/users", (req, res) => {
-  res.send(users);
-});
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
