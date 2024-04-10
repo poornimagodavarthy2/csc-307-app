01836dada3a1cf9030a57baaa114a36a17f4cc59
Poornima <poornima.godavarthy@gmail.com>
2024-04-10 10:59:59 -0700

    Exercise 2: My First REST API

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -37,10 +37,44 @@ const users = {
 const findUserByName = (name) => {
   return users["users_list"].filter((user) => user["name"] === name);
 };
+// get all users
+app.get("/users", (req, res) => {
+  const { name, job } = req.query;
+
+  if (name && job) {
+    // Both name and job parameters are provided
+    const filteredUsers = users.users_list.filter(
+      (user) => user.name === name && user.job === job
+    );
+    res.json({ users_list: filteredUsers });
+  } else if (name) {
+    // Only name parameter is provided
+    const filteredUsers = users.users_list.filter((user) => user.name === name);
+    res.json({ users_list: filteredUsers });
+  } else if (job) {
+    // Only job parameter is provided
+    const filteredUsers = users.users_list.filter((user) => user.job === job);
+    res.json({ users_list: filteredUsers });
+  } else {
+    // No parameters provided, return all users
+    res.json(users);
+  }
+});
 
 const findUserById = (id) =>
   users["users_list"].find((user) => user["id"] === id);
 app.use(express.json());
+
+const addUser = (user) => {
+  users["users_list"].push(user);
+  return user;
+};
+
+app.post("/users", (req, res) => {
+  const userToAdd = req.body;
+  addUser(userToAdd);
+  res.send();
+});
 
 app.get("/users/:id", (req, res) => {
   const id = req.params["id"]; //or req.params.id
@@ -50,6 +84,29 @@ app.get("/users/:id", (req, res) => {
   } else {
     res.send(result);
   }
+});
+
+app.delete("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  const index = users.users_list.findIndex((user) => user.id == id);
+  if (index !== -1) {
+    users.users_list.splice(index, 1);
+    res.status(200).send("User deleted successfully");
+  } else {
+    res.status(404).send("User not found");
+  }
+});
+
+app.get("/users", (req, res) => {
+  const { name, job } = req.query;
+  let filteredUsers = users.users_list;
+  if (name) {
+    filteredUsers = filteredUsers.filter((user) => user.name === name);
+  }
+  if (job) {
+    filteredUsers = filteredUsers.filter((user) => user.job === job);
+  }
+  res.json({ users_list: filteredUsers });
 });
 
 app.listen(port, () => {
